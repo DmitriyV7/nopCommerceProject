46bfd67dbfa8141497882dc038a68a48797edb94
DmitriyV7 <81771691+DmitriyV7@users.noreply.github.com>
2021-04-06 15:08:36 -0500

    added

---
+++ run.bat
@@ -0,0 +1,1 @@
+pytest -s -v -n=2 -m "sanity or regression" --capture=tee-sys --html=./Reports/reporttest.html testCases/ --browser chrome
--- testCases/dummu.py
+++
@@ -1,5 +0,0 @@
-from selenium import webdriver
-import time
-driver=webdriver.Chrome()
-driver.maximaze_window()
-driver.get("www.google.com")
